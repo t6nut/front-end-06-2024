0d1e8121dc3bfc0159b67b988a3c757991bf8f46
Tõnu T. <t6nut@users.noreply.github.com>
2024-06-17 16:05:17 +0300

    first

---
+++ veebipood/src/components/Menyy.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+
+function Menyy() {
+  return (
+    <div>
+        <Link to="avaleht">
+        <img className="pilt" src="https://dalton.ee/wp-content/uploads/2019/10/nobercar1.jpg" alt="" />
+      </Link>
+
+      <Link to="/ostukorv">
+        <button>Ostukorvi</button>
+      </Link>
+
+      <Link to="/lisa-toode">
+        <button>Lisa toode</button>
+      </Link>
+
+      <Link to="/halda-tooteid">
+        <button>Halda tooteid</button>
+      </Link>
+
+      <Link to="/esindused">
+        <button>Esindused</button>
+      </Link>
+
+      <Link to="/kinkekaardid">
+        <button>Kinkekaardid</button>
+      </Link>
+    </div>
+  )
+}
+
+export default Menyy
